bef071b6fb27339ed838ed3442370653bc1326bc
Sojung Im <56745491+sojung32@users.noreply.github.com>
2023-03-03 17:18:30 +0900

    2023-03-03

---
+++ baekjoon/24262_알고리즘_수업-알고리즘의_수행_시간1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        
+        System.out.println(n/n);
+        System.out.println(0);
+
+        scanner.close();
+    }
+}
