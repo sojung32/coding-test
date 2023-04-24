193e2289cb24e322b33ebac9ab7ce79b3dbc6c0d
Sojung Im <56745491+sojung32@users.noreply.github.com>
2023-04-24 15:07:40 +0900

    Create 164670_조건에_맞는_사용자_정보_조회하기_oracle.sql

---
+++ programmers/164670_조건에_맞는_사용자_정보_조회하기_oracle.sql
@@ -0,0 +1,20 @@
+SELECT
+    USER_ID
+    , NICKNAME
+    , CITY || ' ' || STREET_ADDRESS1 || ' ' || STREET_ADDRESS2 AS 전체주소
+    , REGEXP_REPLACE(TLNO, '(.{3})(.+)(.{4})', '\1-\2-\3') AS 전화번호
+FROM
+    USED_GOODS_USER UGU
+LEFT OUTER JOIN (
+    SELECT
+        COUNT(1) DEAL
+        , WRITER_ID
+    FROM
+        USED_GOODS_BOARD
+    GROUP BY
+        WRITER_ID
+) UGB ON UGU.USER_ID = UGB.WRITER_ID
+WHERE
+    UGB.DEAL >= 3
+ORDER BY
+    USER_ID DESC
